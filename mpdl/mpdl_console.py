06ebf97555c9cfe23292ff81bb54400dff70e598
Leopold Tal G <leopold.tal.dg@gmail.com>
2019-08-27 21:30:48 +0200

    add command line arguments

--- mpdl/mpdl_console.py
+++ mpdl/mpdl_console.py
@@ -1,14 +1,44 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from .defaults import DEFAULT_CANVAS_SIZE, DEFAULT_BORDER_WIDTH
+import argparse
+from .defaults import DEFAULT_CANVAS_SIZE, DEFAULT_BORDER_WIDTH, DEFAULT_OUT_FILE
 from .mpdl import run
 
 def main():
-	# TODO: read source file from args
-	source = 'v25 h71 v93 h50 c4 c1 c1 c2 h71 c3 h44 c1 c1'
-	image = run(source, DEFAULT_CANVAS_SIZE, DEFAULT_BORDER_WIDTH)
-	print(image)
+	parser = argparse.ArgumentParser(
+		description = "Mondrian Painting Description Language interpreter"
+	)
+	parser.add_argument('source_file', help = 'path to MPDL source')
+	parser.add_argument(
+		'-o', '--out',
+		default = DEFAULT_OUT_FILE,
+		help = 'path to output file (default "%s")'
+			% (DEFAULT_OUT_FILE,)
+	)
+	parser.add_argument(
+		'-s', '--size',
+		type = int,
+		default = DEFAULT_CANVAS_SIZE,
+		help = 'size of square output image in pixels (default %dpx)'
+			% (DEFAULT_CANVAS_SIZE,)
+	)
+	parser.add_argument(
+		'-b', '--border',
+		type = int,
+		default = DEFAULT_BORDER_WIDTH,
+		help = 'thickness of border in pixels (default %dpx)'
+			% (DEFAULT_BORDER_WIDTH,)
+	)
+	
+	args = parser.parse_args()
+	
+	with open(args.source_file) as h_in:
+		source = h_in.read()
+		image = run(source, args.size, args.border)
+	
+	with open(args.out, 'w') as h_out:
+		h_out.write(image)
 
 if __name__ == '__main__':
 	main()
